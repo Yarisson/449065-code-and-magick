09a64090073a70d6cda30fe4a6f9ff8322ba717b
Yarisson <gemini8811@gmail.com>
2018-01-31 23:53:44 +0300

    создание файла stat.js и описание рисования квадратика

---
+++ js/stat.js
@@ -0,0 +1,23 @@
+var canvas = document.getElementById('canvas');
+
+var ctx = canvas.getContext('2d');
+
+var window {
+  renderStatistics: function (ctx, names, times) {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+    ctx.fillRect(90, 0, 420, 270);
+
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(100, 10, 420, 270);
+  }
+};
+
+  window.renderStatistics : function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(90, 0, 420, 270);
+
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(100, 10, 420, 270);
+
+  };
+
