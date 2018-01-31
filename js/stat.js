3326f1a66351e6df643c3ec85a26d497b147dc6d
Yarisson <gemini8811@gmail.com>
2018-02-01 01:34:13 +0300

    правки в файл stat

--- js/stat.js
+++ js/stat.js
@@ -1,18 +1,4 @@
-var canvas = document.getElementById('canvas');
-
-var ctx = canvas.getContext('2d');
-
-var window {
-  renderStatistics: function (ctx, names, times) {
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-    ctx.fillRect(90, 0, 420, 270);
-
-    ctx.fillStyle = '#fff';
-    ctx.fillRect(100, 10, 420, 270);
-  }
-};
-
-  window.renderStatistics : function (ctx, names, times) {
+var renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(90, 0, 420, 270);
 
@@ -21,3 +7,6 @@ var window {
 
   };
 
+
+
+console.log (window.fireballSize);
