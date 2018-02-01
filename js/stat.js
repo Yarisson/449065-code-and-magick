e46699e6df88a55419b96defc99cd791664d53fe
Yarisson <gemini8811@gmail.com>
2018-02-01 05:43:16 +0300

    3 гистограммы тест

--- js/stat.js
+++ js/stat.js
@@ -3,6 +3,11 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
+var GAP_GISTO_X = 40;
+var GAP_GISTO_Y = 40;
+var GAP_GISTO_TEXT = 20;
+var GISTO_WIDTH = 40;
+var GISTO_STEP = GISTO_WIDTH + 50;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -18,7 +23,17 @@ var renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', 235, 30);
   ctx.fillText('Список результатов:', 220, 50);
 
-  
+  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  ctx.fillText('Вы', CLOUD_X + GAP_GISTO_X, CLOUD_Y + CLOUD_HEIGHT - GAP_GISTO_TEXT);
+  ctx.fillRect(CLOUD_X + GAP_GISTO_X, CLOUD_Y + CLOUD_HEIGHT - GAP_GISTO_Y, GISTO_WIDTH, -100);
+
+  ctx.fillStyle = '#00F';
+  ctx.fillText('Иван', CLOUD_X + GAP_GISTO_X + GISTO_STEP, CLOUD_Y + CLOUD_HEIGHT - GAP_GISTO_TEXT);
+  ctx.fillRect(CLOUD_X + GAP_GISTO_X + GISTO_STEP, CLOUD_Y + CLOUD_HEIGHT - GAP_GISTO_Y, GISTO_WIDTH, -120);
+
+  ctx.fillText('Юлия', CLOUD_X + GAP_GISTO_X + GISTO_STEP + GISTO_STEP, CLOUD_Y + CLOUD_HEIGHT - GAP_GISTO_TEXT);
+  ctx.fillRect(CLOUD_X + GAP_GISTO_X + GISTO_STEP + GISTO_STEP, CLOUD_Y + CLOUD_HEIGHT - GAP_GISTO_Y, GISTO_WIDTH, -80);
+
   };
 
 
