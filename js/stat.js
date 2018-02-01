a115194088bbcda16e0959f827fd66c20e9aa7dc
Yarisson <gemini8811@gmail.com>
2018-02-01 05:00:39 +0300

    фон с тенью и текст - объявление победы

--- js/stat.js
+++ js/stat.js
@@ -1,12 +1,19 @@
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
 var renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(90, 0, 420, 270);
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, 100, 10, '#fff');
 
-  ctx.fillStyle = '#fff';
-  ctx.fillRect(100, 10, 420, 270);
-
+  ctx.font = '16px PT Mono';
+  ctx.fillStyle = '#000';
+  ctx.fillText('Ура вы победили!', 235, 30);
+  ctx.fillText('Список результатов:', 220, 50);
   };
 
 
-
-console.log (window.fireballSize);
